3d4c8bb443c83963a9c95478b60abc4436e6163d
Sai Krishna <skris4291@gmail.com>
2021-10-27 21:55:49 +0530

    feat: add logo

--- pages/api/og.ts
+++ pages/api/og.ts
@@ -6,7 +6,7 @@ import font2base64 from 'node-font2base64';
 const _fontData = font2base64.encodeToDataUrlSync(process.cwd() + '/src/fonts/Inter-Regular.ttf');
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
-    const { title } = req.query;
+    const { title, logoUrl } = req.query;
     const image = await nodeHtmlToImage({
         html: `<html> <head>
         <style>
@@ -16,21 +16,35 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             font-family: 'Inter';
             width: 1200px;
             height: 627px;
+          }
+
+          .root-container {
+            width: 100%;
+            height: 100%;
             display: flex;
             flex-direction: column;
             align-items: center;
             justify-content: center;
           }
 
-          .title {
-              font-size: 48px;
-              font-weight: bold;
-              letter-spacing: -0.005em;
+          .logo {
+              width: 75px;
+              height: 75px;
+              object-fit: contain;
+              margin-bottom: 25px;
           }
+
+          .title {
+            font-size: 48px;
+            font-weight: bold;
+            letter-spacing: -0.005em;
+        }
         </style>
-      </head><body><div><span class="title">${
-          title || 'title not provided'
-      }</span></div></body></html>`,
+      </head><body>
+      <div class="root-container">
+      <img src="${logoUrl}" class="logo" />
+      <span class="title">${title || 'title not provided'}</span>
+      </div></body></html>`,
     });
     res.writeHead(200, { 'Content-Type': 'image/png' });
     res.end(image, 'binary');
